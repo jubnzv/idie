12037ad9dc664b4da65bd8d8b1f2da4de7dc8e80
whoami <whoami@idie.ru>
2016-10-29 17:57:17 +0300

    Rework views and add pagination.

--- board/views.py
+++ board/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 
 import datetime
@@ -25,56 +27,59 @@ class IndexView(TemplateView):
         return context
 
 
-class BoardView(TemplateView):
+class BoardView(ListView):
     """ List of threads for selected board """
     template_name = 'board/board.html'
+    # TODO: Hardcoded paginate_by number. Use config.ini here later~ 
+    paginate_by = 5
+    model = Thread
+    context_object_name = 'thread_list'
+    ordering = '-bump_date'
+
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, slug=self.kwargs['board_slug'])
+        return Thread.objects.filter(board=self.board).order_by(self.ordering)
 
     def get_context_data(self, **kwargs):
         context = super(BoardView, self).get_context_data(**kwargs)
-        this_board = Board.objects.get(slug=self.kwargs['board_slug'])
-
-        context['this_board_name'] = this_board.name
-        context['this_board_desc'] = this_board.desc
-
-        context['is_ro'] = this_board.is_ro
-
-        threads = Thread.objects.filter(board=this_board).order_by('-bump_date')
-        context['threads_list'] = threads
-
+        context['board'] = self.board
         return context
 
 
-class ThreadView(SuccessMessageMixin, CreateView):
-    """ List of posts for selected thread + posting form."""
+class ThreadView(CreateView, SuccessMessageMixin):
+    """ Manages list of posts for current thread and reply form."""
     template_name = 'board/thread.html'
     success_message = "Post successfully created!"
     form_class = PostForm
+    ordering = 'slug'
+    context_object_name = 'post_list'
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        self.object = None
+        return super(ThreadView, self).get(request, *args, **kwargs)
 
     def get_queryset(self):
-        return Post.objects.all()
+        """Returns post list for current thread."""
+        thread = get_object_or_404(Thread, slug=self.kwargs['thread_slug'])
+        return Post.objects.filter(thread=thread).order_by(self.ordering)
 
     def get_context_data(self, **kwargs):
         context = super(ThreadView, self).get_context_data(**kwargs)
-        this_thread = Thread.objects.filter(slug=self.kwargs['thread_slug'])
-        posts = Post.objects.filter(thread=this_thread)
-
-        context['posts_list'] = posts
-
+        context['post_list'] = self.get_queryset() # TODO: ugly~
         return context
 
     def form_valid(self, form, **kwargs):
+        # TODO: get values from context - ?
         post = form.instance
 
         board_slug = self.kwargs['board_slug']
         board = Board.objects.get(slug=board_slug)
-
         thread_slug = self.kwargs['thread_slug']
         thread = Thread.objects.get(slug=thread_slug, board=board)
 
         post.pub_date = datetime.datetime.now()
         post.thread_id = thread.id
-
-        print("IMAGE:: " + str(post.image))
 
         # Update bump date for thread
         if post.is_sage is False:
@@ -117,8 +122,6 @@ class CreateThreadView(SuccessMessageMixin, CreateView):
         post.is_op_post = True
         post.is_sage = False
         post.thread = thread
-
-        print("DEBUG:: " + str(post.image))
 
         form.instance = post
         return super(CreateThreadView, self).form_valid(form)
